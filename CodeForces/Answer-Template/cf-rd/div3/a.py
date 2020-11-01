bc8a854a8b6f68c718efcb70cd3388c5eccfe679
Cosert <mr.cose@yahoo.com>
2020-11-02 00:08:27 +0800

    commit

--- CodeForces/Answer-Template/cf-rd/div3/a.py
+++ CodeForces/Answer-Template/cf-rd/div3/a.py
@@ -1,8 +1,9 @@
 import sys
 
+def solve() : 
+    
+
 while True : 
     line=sys.stdin.readline()
     if not line : break
-    for cas in range(int(line)) : 
-        
-    
+    for cas in range(int(line)) : solve()
